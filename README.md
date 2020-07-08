fcbda90886e1d513c890aa48ca365978892e2f27
CorentinJak <corentinjacob44600@gmail.com>
2020-07-08 19:52:19 +0200

    ADD Heroku setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # bot
 discord bot : harry potter themed, allows users to get a random chocolate frog card or a random jelly bean
+
+# heroku setup
+After creating an app on Heroku website, use automatical webhook with the git and in Settings > Global Vars setup the following env variables :
+- BOT_TOKEN : (discord bot token)
+- PREFIX : (bot command prefix)
+- STATUS : prod
--- main.js
+++ main.js
@@ -1,68 +1,78 @@
 const Discord = require('discord.js')
 const fs = require('fs');
-const {
-	prefix,
-	token
-} = require('./config.json');
+
+if (process.env.STATUS == 'prod') {
+    const {
+        prefix = process.env.PREFIX,
+            token = process.env.BOT_TOKEN
+    }
+    console.log('Prod environnement')
+} else {
+    const {
+        prefix,
+        token
+    } = require('./config.json');
+    console.log('config.json used');
+}
 
 // add all commands from the files in commands
 const bot = new Discord.Client()
 bot.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
+    const command = require(`./commands/${file}`);
 
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
-	bot.commands.set(command.name, command);
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    bot.commands.set(command.name, command);
 }
 
 //add cooldowns to avoid spam
 const cooldowns = new Discord.Collection();
 
-bot.on('ready', function () {
-	console.log("yop")
+bot.on('ready', function() {
+    console.log("bot started")
 })
 
 bot.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
 
-	const command = bot.commands.get(commandName) ||
-		bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-	if (!command) return;
-	if (command.args && !args.length) {
-		return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-	}
-	//cooldowns
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
+    const command = bot.commands.get(commandName) ||
+        bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    if (!command) return;
+    if (command.args && !args.length) {
+        return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+    }
+    //cooldowns
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
 
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
 
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-		}
-	}
-	timestamps.set(message.author.id, now);
-	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    }
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
-	try {
-		console.log(commandName);
-		command.execute(message, args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+    try {
+        console.log(commandName);
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
 });
 
 bot.login(token);
